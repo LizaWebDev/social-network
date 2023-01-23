e260a1f0bf6f40c857bebd0760e4e415f576779d
LizaWebDev <e.kuntsevich03@gmail.com>
2023-01-23 20:18:44 +0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home </a>
+            <a href="#s">News Feed </a>
+            <a href="#s">Messages</a>
+        </div>
+    );
+}
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
+    );
 }
 
 export default App;

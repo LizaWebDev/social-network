ef56f84f99bb9783e33216f3b7ff4e44ec971d88
LizaWebDev <e.kuntsevich03@gmail.com>
2023-01-23 21:16:53 +0300

    Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Technologies from "./Technologies";
+import Footer from "./Footer";
 
 function App() {
     return (
         <div className="App">
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }

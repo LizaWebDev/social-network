cffd5cdcdcb2550d41b84f9116c06db8e55c1af8
LizaWebDev <e.kuntsevich03@gmail.com>
2023-02-12 15:54:55 +0300

    css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className={'header'}>
+        <header className={s.header}>
             <img
                 src="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-holiday-summer-logo-png-image_711561.jpg"
                 alt=""/>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={'content'}>
+        <div className={s.content}>
             <div>
                 <img
                     src="https://w0.peakpx.com/wallpaper/551/932/HD-wallpaper-nature-supreme-lakes-mountains-reflections-nature.jpg"
@@ -11,12 +12,12 @@ const Profile = () => {
             <div>
                 ava + description
             </div>
-            <div>
+            <div className={s.posts}>
                 My posts
                 <div>New post</div>
                 <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
+                    <div className={s.item}>Post 1</div>
+                    <div className={s.item}>Post 2</div>
                 </div>
             </div>
         </div>
